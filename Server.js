dcf99f204c0e277211d57f7edbe0d5b65af6c611
G4uravJha302 <mr.gaurav.aj@gmail.com>
2025-08-23 20:56:58 +0530

    Backend file changing

--- Server.js
+++ Server.js
@@ -11,14 +11,38 @@ connectToMongo();
 const port = process.env.PORT || 8081;
 const app = express();
 
-app.use(cors({
-  origin: [
-    "http://localhost:5173",               // local dev
-    "https://task-manager-frontend-7mk5.vercel.app"  // vercel deploy
-  ],
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  credentials: true
-}));
+// app.use(cors({
+//   origin: [
+//     "http://localhost:5173",               // local dev
+//     "https://task-manager-frontend-7mk5-3ybev6awq.vercel.app" 
+//      // vercel deploy
+//   ],
+//   methods: ['GET', 'POST', 'PUT', 'DELETE'],
+//   credentials: true
+// }));
+
+
+
+const allowedOrigins = [
+  "http://localhost:5173",                         // local frontend
+  "https://task-manager-frontend-7mk5.vercel.app"  // deployed frontend
+];
+
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      // Allow requests with no origin (like mobile apps or curl)
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.includes(origin)) {
+        return callback(null, true);
+      } else {
+        return callback(new Error("Not allowed by CORS"));
+      }
+    },
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use(cookieParser());
 app.use('/auth', Auth_router);
