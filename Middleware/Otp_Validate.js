98b69a7ecc59e2b9c4cadfa5d098265a503b37a2
G4uravJha302 <mr.gaurav.aj@gmail.com>
2025-08-24 17:13:41 +0530

    Backend file changing

--- Middleware/Otp_Validate.js
+++ Middleware/Otp_Validate.js
@@ -23,8 +23,9 @@ export const Otpverify = async (req, res) => {
 
     res.cookie("token", token, {
       httpOnly: true,
-      secure: process.env.NODE_ENV === "production",
-      sameSite: "none",
+      secure: true,
+      sameSite: "lax",
+      maxAge: 16 * 24 * 60 * 60 * 1000,
     });
     return res.status(200).json({ message: "OTP verified successfully", user: { id: user._id, email: user.email } });
   } catch (err) {
